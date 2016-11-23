c8633eb1b89d0a852d5100cc74028d48362de9d8
gallenhu <gallenhu@foxmail.com>
2016-11-23 18:11:42 +0800

    Support for download default template

---
+++ config/repo.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'react': 'github:hasakijs/hasaki-template-react',
+  'vue': 'github:hasakijs/hasaki-template-vue',
+  'angular2': 'github:hasakijs/hasaki-template-angular2'
+};
